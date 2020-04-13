294708636c6561c2b1cb6eb274ec57796df1831c
lukhaza <ntut@hotmail.com>
2020-04-13 14:48:23 +0200

    remove last "/" in hostApi

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     hostUi: 'lukhanyo.ml',
-    hostApi: 'lukhanyo.ml/api/',
+    hostApi: 'lukhanyo.ml/api',
   };
 //    localhost:4200 ----> lukhanyo.ml
 //    localhost:3000 ----> lukhanyo.ml/api/
